ca8373d923f5515a2a012d1b74deee94ef65c479
Martyn Hollins <martyn.hollins@debeersgroup.com>
2025-02-24 11:49:34 +0000

    Adding some complexity.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+
+
+string key = "12345"; 
+
+
+for (int i = 0; i < 10; i++)
+{
+    if(true)
+    {
+        if(true)
+        {
+            if(true)
+            {
+                if(true)
+                {
+                    if(true)
+                    {
+                        if(i % 2 == 0)
+                        {
+                            Console.WriteLine("Hello again"); ; 
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
 for(int i =0; i < 10; i++)
 {
     Console.WriteLine("Hello, World!");
